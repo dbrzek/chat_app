d725ba0f2622e36c508485be4d9f26bcc92b28c4
Damian <brzek.damian@gmail.com>
2022-08-29 13:38:12 +0200

    Adding pushnotification. Application need - firebase deploy(in teminal) - to finish

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,11 @@ import './screens/splash_screen.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp();
+   runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
